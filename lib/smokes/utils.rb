278b14414da48022594df2f0a7b443588acefc40
Suyesh Bhandari <suyeshb@gmail.com>
2018-08-15 16:42:25 -0400

    .

--- lib/smokes/utils.rb
+++ lib/smokes/utils.rb
@@ -2,6 +2,7 @@ module Smokes
   module Utils
     def check_yaml(filename)
       unless YAML.dump(YAML.load_file(filename)) == File.read(filename).gsub(/\s*#.*/, '')
+        output = []
         r, io = IO.pipe
         fork do
           system('yaml-lint filename', out: io, err: :out)
