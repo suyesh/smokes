7b6f348fd834c5f8c9fa9b47a212b6ddc87262ad
Suyesh Bhandari <suyeshb@gmail.com>
2018-08-14 23:18:29 -0400

    .

--- lib/smokes.rb
+++ lib/smokes.rb
@@ -2,7 +2,6 @@ require 'smokes/version'
 require 'thor'
 require 'colorize'
 require 'smokes/cli'
-require 'smokes/init'
 
 module Smokes
 end
--- lib/smokes/cli.rb
+++ lib/smokes/cli.rb
@@ -5,5 +5,9 @@ module Smokes
   class Cli < Thor
     include Thor::Actions
 
+    desc "Initialize new test"
+    def init
+      empty_directory ARGV[1]
+    end
   end
 end
