1a6f4455d22ebcc9bcf72c6fe83cf9877f25aed4
Suyesh Bhandari <suyeshb@gmail.com>
2018-08-14 23:42:03 -0400

    .

--- lib/smokes/cli.rb
+++ lib/smokes/cli.rb
@@ -4,17 +4,17 @@ require 'colorize'
 module Smokes
   class Cli < Thor
     include Thor::Actions
-    argument :url
 
     def self.source_root
       File.dirname __FILE__
     end
 
     desc 'init new_test_project', 'Initialize new test project'
-    def init(name)
+    def init(name, url)
+      opts = {url: url}
       empty_directory name
       empty_directory "#{name}/Tests"
-      template 'templates/main.tt', "#{name}/main.yaml"
+      template 'templates/main.tt', "#{name}/main.yaml", opts
     end
   end
 end
