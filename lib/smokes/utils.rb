1b00df3996f7601eaa56e42a872da6563edfde78
Suyesh Bhandari <suyeshb@gmail.com>
2018-08-16 14:44:15 -0400

    .

--- lib/smokes/utils.rb
+++ lib/smokes/utils.rb
@@ -1,7 +1,17 @@
 module Smokes
   module Utils
     def symbolize_hash(hash)
-      hash.each_with_object({}) { |(k, v), memo| memo[k.to_sym] = v }
+      hash.each_with_object({}) do |(key, value), result|
+        new_key = case key
+                  when String then key.to_sym
+                  else key
+                  end
+        new_value = case value
+                    when Hash then symbolize_keys(value)
+                    else value
+                    end
+        result[new_key] = new_value
+      end
     end
   end
 end
