df828557863068b5141962d73f65780243a7e264
Suyesh Bhandari <suyeshb@gmail.com>
2018-08-17 12:11:18 -0400

    .

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-{
-:class => 'ClassName',
-:class_name => 'ClassName',
-:css => 'CssSelector',
-:id => 'Id',
-:link => 'LinkText',
-:link_text => 'LinkText',
-:name => 'Name',
-:partial_link_text => 'PartialLinkText',
-:tag_name => 'TagName',
-:xpath => 'Xpath',
-}
+## `document` assert accepts
+
+1.  current_url
+2.  visible? `This does not accept any parameter`
+3.  title
+
+## `document` action accepts
+
+1.  close `This does not accept any parameter`
+2.  execute_acync_script
+3.  execute_script
+4.  quit `This does not accept any parameter`
+
+## `element` assert accepts
+
+1.  class
+2.  class_name
+3.  css
+4.  id
+5.  link
+6.  link_text
+7.  name
+8.  partial_link_text
+9.  tag_name
+10. xpath
--- lib/smokes/document_handler.rb
+++ lib/smokes/document_handler.rb
@@ -1,11 +1,127 @@
 module Smokes
+  # Runs for Document Test
   class DocumentHandler
-    def initialize(test)
+    ACTIONS = %w[close execute_acync_script execute_script quit].freeze
+    ASSERT  = %w[current_url visible? title].freeze
+
+    def initialize(_test, browser, wait)
+      @pastel = Pastel.new
+      @browser = browser
+      @wait = wait
       @test = test
     end
 
     def run
-      puts @test
+      parse_attributes
+    end
+
+    private
+
+    def parse_attributes
+      name
+      document
+      check_action
+      validate_assert
+      validate_assertion
+      run_assertion
+    end
+
+    def name
+      @name = validate_attribute(@test, 'name')['name']
+    end
+
+    def document
+      @document = validate_attribute(@test, 'document', @name)['document']
+    end
+
+    def validate_attribute(test, attribute, name = nil)
+      unless test.key?(attribute)
+        @pastel.white.on_red.bold("#{name || 'Test'} is missing '#{attribute}'")
+        abort
+      end
+      test
+    end
+
+    def validate_assertion
+      if invalid_assertions || !ASSERT.include?(assertions[0])
+        @pastel.white.on_red.bold("Invalid Assertion for #{@name}")
+        abort
+      end
+      @target = assertion[0]
+      @assertion = assertion[1]
+    end
+
+    def run_assertion
+      result = @browser.send(@target)
+      if result == @assertion
+        @pastel.white.on_green.bold("#{@name} Passed successfully. ")
+      else
+        @pastel.white.on_red.bold("#{@name} Failed. ")
+      end
+    rescue StandardError
+      puts "Something went wrong while running the test #{@name}".colorize(:red)
+    end
+
+    def validate_assert
+      @assert = validate_attribute(@document, 'assert', @name)['assert']
+    end
+
+    def invalid_assertion
+      assertions.length != 2
+    end
+
+    def assertions
+      @assert.split('=')
+    end
+
+    def check_action
+      if @document.key?('action')
+        @action = @document.key?('action')
+        run_actions
+      end
+    end
+
+    def run_actions
+      action = @action.split('=')
+      valid_action(action)
+      if !close_or_quit(action[0])
+        @pastel.white.on_red.bold("#{action[0]} is not a valid action for #{@name}")
+        abort
+      elsif close_or_quit(action[0])
+        begin
+          @browser.send(action[0])
+          @pastel.white.on_green.bold("#{action[0]} was successfully performed for #{@name}. Passed")
+        rescue StandardError
+          @pastel.white.on_red.bold("#{action[0]} Could not be performed for #{@name}. Failed")
+        end
+      elsif action_requiring_parameter(action)
+        begin
+         @browser.send(action[0], action[1])
+         @pastel.white.on_green.bold("#{action[0]} was successfully performed for #{@name}. Passed")
+       rescue StandardError
+         @pastel.white.on_red.bold("#{action[0]} Could not be performed for #{@name}. Failed")
+       end
+      end
+    end
+
+    def close_or_quit(action)
+      action.length == 1 && %w[close quit].include?(action)
+    end
+
+    def valid_action(action)
+      unless ACTIONS.include?(action)
+        @pastel.white.on_red.bold("Invalid Action type for #{@name}")
+        abort
+      end
+    end
+
+    def action_requiring_parameter(action)
+      valid = %w[execute_acync_script execute_script].include?(action[0]) && action.length != 2
+      unless valid
+        @pastel.white.on_red.bold("#{action[0]} is missing parameter for #{@name}. Failed")
+        abort
+      end
+      valid
     end
   end
 end
